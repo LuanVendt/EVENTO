b80f7610aa29484b60ddfaa6cf61e03b785d2f3b
Luan Vendt <luan.vendt@bitize.com.br>
2025-02-03 10:41:55 -0300

    feat: enhance event pages with dynamic city titles and add H1 component for consistent styling style: implement custom scrollbar styles for better user experience fix: wrap logo in a link for improved navigation

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+
+import H1 from "@/components/h1";
+import { usePathname } from "next/navigation";
+
 export default function EventsPage() {
-  return <div>EventsPage</div>;
+  const pathName = usePathname();
+
+  const city = pathName.split("/")[2];
+
+  return (
+    <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
+      <H1>Events in {city}</H1>
+    </main>
+  );
 }
