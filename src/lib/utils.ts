ebac52d3d6c2404c9dd2a5a1ecf9cdc499c0a84b
Luan Vendt <luan.vendt@bitize.com.br>
2025-02-04 15:53:44 -0300

    feat: refactor event fetching to use server-only utilities and update dependencies

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,5 @@
 import clsx, { ClassValue } from "clsx";
-import { unstable_cache } from "next/cache";
-import { notFound } from "next/navigation";
 import { twMerge } from "tailwind-merge";
-import prisma from "./db";
 
 export function cn(...classes: ClassValue[]) {
   return twMerge(clsx(classes));
@@ -11,31 +8,3 @@ export function cn(...classes: ClassValue[]) {
 export function capitalize(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
-
-export const getEvents = unstable_cache(async (city: string, page = 1) => {
-  const events = await prisma.eventoEvent.findMany({
-    where: { city: city === "all" ? undefined : capitalize(city) },
-    orderBy: { date: "asc" },
-    take: 6,
-    skip: (page - 1) * 6,
-  });
-
-  const totalCount = await prisma.eventoEvent.count({
-    where: { city: city === "all" ? undefined : capitalize(city) },
-  });
-
-  return {
-    events,
-    totalCount,
-  };
-});
-
-export const getEvent = unstable_cache(async (slug: string) => {
-  const event = await prisma.eventoEvent.findUnique({
-    where: { slug },
-  });
-
-  if (!event) return notFound();
-
-  return event;
-});
